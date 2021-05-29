c8700504ddc7a88d9541a26b97a3378c1aa1eb1c
ZaidZaim <43517319+ZaidZaim@users.noreply.github.com>
2021-05-29 15:26:10 +0200

    cleanup

--- Palmyra/Assets/Scripts/DestroyMonument.cs
+++ Palmyra/Assets/Scripts/DestroyMonument.cs
@@ -12,23 +12,13 @@ public class DestroyMonument : MonoBehaviour
     public Animator monumentanim;
     public GameObject ornaments;
     public GameObject[] stoneglitch;
-    bool isBeignDestroyed = false;
-    bool isBeignRebuilt = false;
     public UnityEvent OnMonumentDestroyed;
     public GameObject[] stoneToSnap;
     public GameObject[] stoneOfAnim;
     public GameObject[] stoneMainLocation;
-    PartAssemblyController partAssembly;
-    //public GameObject interact1;
-
-    //public BoxCollider destroyButton;
-    //public BoxCollider buildButton;
     private GameObject destroyButton;
     private GameObject rebuildButton;
     
-
-
-
     void Start()
     {
         destroyButton = GameObject.FindGameObjectWithTag("Destroy");
@@ -52,69 +42,48 @@ public class DestroyMonument : MonoBehaviour
 
     public void Destroy()
     {
-        //interact1.In.enabled = false;
-        //destroyButton.enabled = false;
-        
-        //OnRebuildFinished();
+
         monumentanim.SetTrigger("destroy");
         ornaments.SetActive(false);
         destroyButton.GetComponent<Interactable>().enabled = false;
         destroyButton.GetComponent<PressableButtonHoloLens2>().enabled = false;
         StartCoroutine(StoneToSnap());
         StartCoroutine(TrackDestroyAnim());
-
-        //OnRebuildFinished();
         Debug.Log("Botton Off");
-
-
     }
 
     public void Rebuild()
     {
-
-        //OnDestroyFinished();
-        //buildButton.enabled = false;
         rebuildButton.GetComponent<Interactable>().enabled = false;
         rebuildButton.GetComponent<PressableButtonHoloLens2>().enabled = false;
         StartCoroutine(Waitrebuild());
-
         StartCoroutine(WaittoActive());
-
         Debug.Log("Botton Off"); 
-
     }
     IEnumerator Waitrebuild()
     {
-        //yield return new WaitForSeconds(6.208f);
         monumentanim.SetTrigger("rebuild");
 
         yield return new WaitForSeconds(6.210f);
         ornaments.SetActive(true);
-        //destroyButton.enabled = true;
-        //stoneOfAnim.SetActive(true);
         foreach (GameObject stones in stoneOfAnim )
         {
             stones.SetActive(true);
         }
-        //stoneToSnap.SetActive(false);
+
         foreach (GameObject stones in stoneToSnap)
         {
             stones.SetActive(false);
         }
-        //stoneMainLocation.SetActive(false);
+        
         foreach (GameObject stones in stoneMainLocation)
         {
             stones.SetActive(false);
         }
-        //yield return new WaitForSeconds(6.13f);
-
     }
     IEnumerator TrackDestroyAnim()
     {
         yield return new WaitForSeconds(6.210f);
-        //buildButton.enabled = true;
-        //Monument already destroyed
-        //OnMonumentDestroyed.Invoke();
     }
 
     IEnumerator WaittoActive()
@@ -129,25 +98,20 @@ public class DestroyMonument : MonoBehaviour
     IEnumerator StoneToSnap()
     {
         yield return new WaitForSeconds(6.1f);
-        //stoneToSnap.SetActive(true);
-        //stoneOfAnim.SetActive(false);
-        //stoneMainLocation.SetActive(true);
-
         foreach (GameObject stones in stoneOfAnim)
         {
             stones.SetActive(false);
         }
-        //stoneToSnap.SetActive(false);
+        
         foreach (GameObject stones in stoneToSnap)
         {
             stones.SetActive(true);
             stones.GetComponent<PartAssemblyController>().ResetPlacement();
         }
-        //stoneMainLocation.SetActive(false);
+
         foreach (GameObject stones in stoneMainLocation)
         {
             stones.SetActive(true);
         }
-        //stoneToSnap.GetComponent<PartAssemblyController>().ResetPlacement();
     }
 }
