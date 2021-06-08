d0373c2fd5962fc5713f02411c86a6bcf4b0794a
Rehaan Mazid <38107493+Rehaan1@users.noreply.github.com>
2021-06-08 14:16:31 +0530

    feat: timeline added on globe
    note: need to add photon to TimelineController.cs script

---
+++ Palmyra/Assets/Scripts/TimelineController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineController : MonoBehaviour
+{
+    public List<PlayableDirector> playableDirectors;
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            Play();
+        }
+    }
+
+    public void Play()
+    {
+        //plays all playable directors attached to script
+        foreach(PlayableDirector playableDirector in playableDirectors)
+        {
+            playableDirector.Play();    
+        }
+    }
+}
