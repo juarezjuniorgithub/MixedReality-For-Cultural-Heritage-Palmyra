f34756215845014e6e20a25db579ccd385c78476
Rehaan Mazid <38107493+Rehaan1@users.noreply.github.com>
2021-06-11 20:26:23 +0530

    test: head gaze experiment

---
+++ Palmyra/Assets/Scripts/ExperimentalHeadGaze.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit;
+
+public class ExperimentalHeadGaze : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LogCurrentGazeTarget();
+    }
+
+    void LogCurrentGazeTarget()
+    {
+        if (CoreServices.InputSystem.GazeProvider.GazeTarget)
+        {
+            Debug.Log("User gaze is currently over game object: "
+                + CoreServices.InputSystem.GazeProvider.GazeTarget);
+                if(CoreServices.InputSystem.GazeProvider.GazeTarget.transform.tag == "monument")
+                {
+                    Debug.Log("monument");
+                    CoreServices.InputSystem.GazeProvider.GazeTarget.GetComponent<GazeControl>().StartFloatSequence();
+                }
+        }
+
+    }
+}
