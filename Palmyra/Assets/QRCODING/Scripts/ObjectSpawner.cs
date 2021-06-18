a6412a046f28a3250e3bf9635611d639a4bb5597
Rehaan Mazid <38107493+Rehaan1@users.noreply.github.com>
2021-06-18 22:43:06 +0530

    test: all rotation

--- Palmyra/Assets/QRCODING/Scripts/ObjectSpawner.cs
+++ Palmyra/Assets/QRCODING/Scripts/ObjectSpawner.cs
@@ -17,19 +17,23 @@ public class ObjectSpawner : MonoBehaviour
         Quaternion newRotation;
         var childObj = transform.GetChild(0);
 
-        if(pose.rotation.eulerAngles.z > 2)
-        {
-            newRotation = Quaternion.Euler( pose.rotation.eulerAngles.x, pose.rotation.eulerAngles.y, 0);
-        }
-        else
-        {
-            newRotation = pose.rotation;
-        }
+       // if(pose.rotation.eulerAngles.z > 2)
+       // {
+            newRotation = Quaternion.Euler( 0, 0, 0);
+       // }
+       // else
+      //  {
+       //     newRotation = pose.rotation;
+       // }
 
         childObj.SetPositionAndRotation(pose.position, newRotation); //setting position and rotation
         
-        Debug.Log("----------------ROTATION---------------------------------------");
+        Debug.Log("----------------ROTATION of POSE---------------------------------------");
         Debug.Log("X: "+pose.rotation.eulerAngles.x+", Y: "+pose.rotation.eulerAngles.y+", Z: "+pose.rotation.eulerAngles.z);
+        Debug.Log("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+        Debug.Log("----------------NEW ROTATION---------------------------------------");
+        Debug.Log("X: "+newRotation.eulerAngles.x+", Y: "+newRotation.eulerAngles.y+", Z: "+newRotation.eulerAngles.z);
         Debug.Log("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
         
         childObj.gameObject.SetActive(true);
