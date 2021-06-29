df3a245eef291e1c9962151269c52791cb4432b2
ZaidZaim <43517319+ZaidZaim@users.noreply.github.com>
2021-06-29 19:37:37 +0200

    Adding BaalshaminTemple test & SnapPanel to Table Script

---
+++ Palmyra/Assets/Scripts/ResetScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManaging : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Scene loaded");
+    }
+
+    // Update is called once per frame
+
+    public void ResetScene() 
+    {
+       
+    }
+
+}
