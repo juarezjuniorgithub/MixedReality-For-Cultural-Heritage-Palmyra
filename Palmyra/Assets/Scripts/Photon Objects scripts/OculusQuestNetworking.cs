9adf64c919f4ca6b2f332f484f20b882da72b5e7
lucas-zaubar <lucas@zaubar.com>
2021-07-04 19:06:43 +0200

    Oculus quest offset to match hololens height.

---
+++ Palmyra/Assets/Scripts/Photon Objects scripts/OculusQuestNetworking.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OculusQuestNetworking : MonoBehaviour
+{
+    OVRCameraRig cameraRig = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(cameraRig == null) {
+            cameraRig = FindObjectOfType<OVRCameraRig>();
+            if(cameraRig != null) {
+                SetOculusQuestStuff();
+            }
+        }
+    }
+
+    void SetOculusQuestStuff() {
+        cameraRig.transform.position -= new Vector3(0, 1.5f, 0);
+    }
+}
