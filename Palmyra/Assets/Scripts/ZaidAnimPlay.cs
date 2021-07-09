5e269ce63d5331d8cb89feb328d253707fa8949e
Rehaan Mazid <38107493+Rehaan1@users.noreply.github.com>
2021-07-09 11:08:32 +0530

    feat: zaid anim in temple

---
+++ Palmyra/Assets/Scripts/ZaidAnimPlay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ZaidAnimPlay : MonoBehaviour
+{
+    [SerializeField] PlayableDirector playableDirector;
+
+    public void PlayZaidAnimation()
+    {
+        playableDirector.Play();
+    }
+}
