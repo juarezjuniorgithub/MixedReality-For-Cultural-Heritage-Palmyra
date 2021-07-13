679031a5cc01f81f96b50c56776063b141dc8032
ZaidZaim <43517319+ZaidZaim@users.noreply.github.com>
2021-07-13 20:27:38 +0200

    Setting icons & Photon server "eu"

---
+++ Palmyra/Assets/Scripts/BaalHider.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BaalHider : MonoBehaviour
+{
+    MeshRenderer _renderer;
+    //[SerializeField] MeshRenderer groundRenderer;
+    [SerializeField] GameObject[] images;
+    [SerializeField] Transform parentTransform;
+    private float initialScale;
+    Color color = Color.white;
+    [SerializeField] float maxDelta = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //_renderer = GetComponent<MeshRenderer>();
+        initialScale = parentTransform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float delta = parentTransform.localScale.x - initialScale;
+
+        if(delta > 0) {
+            color.a = Mathf.InverseLerp(maxDelta, 0, delta);
+
+            if(delta > maxDelta)
+            {
+                //groundRenderer.enabled = false;
+                foreach (var item in images)
+                {
+                    item.SetActive(false);
+                }
+            }
+            else
+            {
+                //groundRenderer.enabled = true;
+                foreach (var item in images)
+                {
+                    item.SetActive(true);
+                }
+            }
+
+            //_renderer.material.color = color;
+        }
+    }
+}
