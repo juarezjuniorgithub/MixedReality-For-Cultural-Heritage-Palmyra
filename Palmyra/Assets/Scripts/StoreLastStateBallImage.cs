ccf502643421a2b33d22501670b8aa8cb9f78ad0
Rehaan Mazid <38107493+Rehaan1@users.noreply.github.com>
2021-07-24 11:11:45 +0530

    fix: baalshamin images solve

---
+++ Palmyra/Assets/Scripts/StoreLastStateBallImage.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreLastStateBallImage : MonoBehaviour
+{
+    [SerializeField] GameObject baalBefore;
+    [SerializeField] GameObject baalAfter;
+    bool b1=true;
+    bool b2=false;
+    public GameObject currentState;
+
+    void Start()
+    {
+        currentState = baalBefore;
+    }
+
+    public void ChangeState()
+    {
+        if(b1)
+        {
+            b1=false;
+            currentState = baalAfter;
+            b2=true;
+        }
+        else if(b2)
+        {
+            b2=false;
+            currentState = baalBefore;
+            b1=true;
+        }
+    }
+    
+    public GameObject GetCurrentState()
+    {
+        return currentState;
+    }
+}
