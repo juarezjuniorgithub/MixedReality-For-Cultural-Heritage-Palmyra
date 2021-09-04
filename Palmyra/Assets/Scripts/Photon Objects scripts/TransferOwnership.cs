3159bab06b89805c8ed16a7013523a03d5199f85
lucas martinic <lucas@zaubar.com>
2021-09-04 14:19:19 +0200

    TransferOwnership refactoring

--- Palmyra/Assets/Scripts/Photon Objects scripts/TransferOwnership.cs
+++ Palmyra/Assets/Scripts/Photon Objects scripts/TransferOwnership.cs
@@ -1,8 +1,21 @@
-﻿using Photon.Pun;
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using Photon.Pun;
+using UnityEngine;
 
+[RequireComponent(typeof(ObjectManipulator))]
 public class TransferOwnership : MonoBehaviourPun
 {
-    public void TransferOwnershipWhenGrab() {
+    ObjectManipulator objectManipulator;
+
+    private void Start() {
+        objectManipulator.OnManipulationStarted.AddListener(TransferOwnershipWhenGrab);
+    }
+
+    private void OnDestroy() {
+        objectManipulator.OnManipulationStarted.RemoveListener(TransferOwnershipWhenGrab);
+    }
+
+    public void TransferOwnershipWhenGrab(ManipulationEventData arg0) {
         photonView.TransferOwnership(PhotonNetwork.LocalPlayer);
     }
 }
