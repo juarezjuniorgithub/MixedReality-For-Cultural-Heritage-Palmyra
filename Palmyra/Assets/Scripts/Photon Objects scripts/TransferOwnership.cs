b030835c76eddb3620c9cdff7169521a8157e67e
lucas martinic <lucas@zaubar.com>
2021-09-04 13:06:51 +0200

    Transfer ownership script

---
+++ Palmyra/Assets/Scripts/Photon Objects scripts/TransferOwnership.cs
@@ -0,0 +1,8 @@
+﻿using Photon.Pun;
+
+public class TransferOwnership : MonoBehaviourPun
+{
+    public void TransferOwnershipWhenGrab() {
+        photonView.TransferOwnership(PhotonNetwork.LocalPlayer);
+    }
+}
