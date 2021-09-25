0e532fc963a02433e92b6862bf829fb5a8382a56
lucas martinic <lucas@zaubar.com>
2021-09-25 17:17:18 +0200

    Revert "Revert "Merge branch 'main' of https://github.com/ZaidZaim/MixedReality-For-Cultural-Heritage-Palmyra into main""
    This reverts commit 6e872dc07b6a3e900f3871abf50e5046c1877702.

--- Palmyra/Assets/Scripts/GazeControl.cs
+++ Palmyra/Assets/Scripts/GazeControl.cs
@@ -20,7 +20,7 @@ public class GazeControl : MonoBehaviour
 
     //what is the delays before buttons are set active
     [Tooltip("Delay for buttons and objects to become active")]
-    [SerializeField] float additionalObjectsActivationDelay = 0.5f;
+    [SerializeField] float buttonObjectsActivationDelay = 0.5f;
     // what is the delay before the additional objects are activated
     [SerializeField] float activateOtherAdditionalObjectsDelay = 1f;
     //what is the delay before holders are deactivated
@@ -29,8 +29,8 @@ public class GazeControl : MonoBehaviour
     [SerializeField] GameObject buttons;
     //List of additional gameObjects that need to be set active later
     [SerializeField] List<GameObject> additionalActivators;
-    //or holder GameObjects that needs to be deactivated when additional activators are activated. Eg, the Arck initially and the beauty holder with the dissolve script
-
+    //for holder GameObjects that needs to be deactivated when additional activators are activated. 
+    //Eg the lowpoly beauty holder in the scene
     [Tooltip("For Holder GameObjects")]
     [SerializeField] List<GameObject> holderDeactivators;
 
@@ -247,13 +247,13 @@ public class GazeControl : MonoBehaviour
 
     [PunRPC]
     public void RPC_DeactivateGazeControlSequence() {
-        StartCoroutine(ActivateAdditionalGameObjects());
+        StartCoroutine(ActivateButtonGameObjects());
     }
 
-    //activates additional GameObjects like the buttons
-    IEnumerator ActivateAdditionalGameObjects()
+    //activates button GameObjects
+    IEnumerator ActivateButtonGameObjects()
     {
-        yield return new WaitForSeconds(additionalObjectsActivationDelay);
+        yield return new WaitForSeconds(buttonObjectsActivationDelay);
         SetUpButtons();
     }
 
