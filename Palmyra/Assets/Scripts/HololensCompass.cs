e5edc496c2beeea27f3e2bd9e3d28b1a68d7eba6
lucas martinic <lucas@zaubar.com>
2021-10-01 22:41:41 +0200

    Compass test script

---
+++ Palmyra/Assets/Scripts/HololensCompass.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class HololensCompass : MonoBehaviour
+{
+    [SerializeField] TMPro.TextMeshProUGUI compassText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        compassText.text = Input.compass.magneticHeading.ToString(); 
+    }
+}
