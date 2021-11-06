3ee02a7df2ac0b6b62eb81889396b06c4cf93c89
lucas martinic <42948357+lucas-martinic@users.noreply.github.com>
2021-11-06 13:45:13 +0100

    Themeing class to apply UI mat to every UI object in scene

---
+++ Palmyra/Assets/Scripts/UI/ApplyTheme.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class ApplyTheme : MonoBehaviour
+{
+    public enum Type {UI};
+    public Type type = Type.UI;
+}
---
+++ Palmyra/Assets/Scripts/UI/Themeing.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Themeing : MonoBehaviour
+{
+    public Material uiMaterial;
+
+    private void OnValidate() {
+        ApplyTheme[] themeables = FindObjectsOfType<ApplyTheme>();
+        foreach (var item in themeables) {
+            if(item.type == ApplyTheme.Type.UI) {
+                item.TryGetComponent(out MeshRenderer renderer);
+                if(renderer != null) {
+                    renderer.material = uiMaterial;
+                }
+            }
+        }
+    }
+}
