dcf198c9de94a2dae050151f25d19809b3731ae6
lucas martinic <42948357+lucas-martinic@users.noreply.github.com>
2021-11-06 13:50:57 +0100

    Added ApplyTheme to every Ui object in scene

--- Palmyra/Assets/Scripts/UI/Themeing.cs
+++ Palmyra/Assets/Scripts/UI/Themeing.cs
@@ -5,7 +5,7 @@ public class Themeing : MonoBehaviour
     public Material uiMaterial;
 
     private void OnValidate() {
-        ApplyTheme[] themeables = FindObjectsOfType<ApplyTheme>();
+        ApplyTheme[] themeables = Resources.FindObjectsOfTypeAll<ApplyTheme>();
         foreach (var item in themeables) {
             if(item.type == ApplyTheme.Type.UI) {
                 item.TryGetComponent(out MeshRenderer renderer);
