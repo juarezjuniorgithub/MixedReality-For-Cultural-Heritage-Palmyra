0e9da0dc16b65281418e2f3aab7aeabd3237a79e
lucas martinic <42948357+lucas-martinic@users.noreply.github.com>
2022-01-15 13:47:21 +0100

    TextPane dissapearing when scaling monument up

--- Palmyra/Assets/Scripts/Monument.cs
+++ Palmyra/Assets/Scripts/Monument.cs
@@ -29,9 +29,9 @@ public class Monument : MonoBehaviour
         if(localTextPanel == null)
         {
             Vector3 directionBetweenUserAndMonument = transform.position - GlobalReferences.instance.localUser.transform.position;
-            localTextPanel = Instantiate(textPanelPrefab, transform.position + new Vector3(0,0,0) , transform.rotation).GetComponent<TextPanel>();
-            localTextPanel.transform.SetParent(transform);
-            localTextPanel.Initialize(textPanelTitle, textPanelBody, textPanelImage);
+            localTextPanel = Instantiate(textPanelPrefab, transform.position + -transform.right * 0.35f + transform.up * 0.15f, transform.rotation).GetComponent<TextPanel>();
+            localTextPanel.transform.SetParent(transform.GetChild(0).transform);
+            localTextPanel.Initialize(textPanelTitle, textPanelBody, textPanelImage, transform.GetChild(0).transform);
         }
         localTextPanel.gameObject.SetActive(true);
         localTextPanel.PlayAnimation();
--- Palmyra/Assets/Scripts/UI/TextPanel.cs
+++ Palmyra/Assets/Scripts/UI/TextPanel.cs
@@ -1,25 +1,67 @@
 ﻿using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System.Collections;
 
 public class TextPanel : MonoBehaviour
 {
     public TextMeshProUGUI title;
     public TextMeshProUGUI body;
+    private Color textColor;
     public RawImage image;
-    [SerializeField] private GameObject background;
-    [SerializeField] private GameObject foreground;
+    [SerializeField] private MeshRenderer background;
+    private Color backgroundColor;
+    [SerializeField] private MeshRenderer foreground;
+    private Color foregroundColor;
     [SerializeField] private Animator animator;
 
-    public void Initialize(string _title, string _body, Texture2D _image)
+    public void Initialize(string _title, string _body, Texture2D _image, Transform _parentScaleReference)
     {
         title.text = _title;
         body.text = _body;
         image.texture = _image;
+        parentScaleReference = _parentScaleReference;
+        backgroundColor = background.material.color;
+        foregroundColor = foreground.material.color;
+        textColor = body.color;
+        StartCoroutine(WaitBeforeSetingInitialScale(_parentScaleReference));
+    }
+
+    IEnumerator WaitBeforeSetingInitialScale(Transform _parentScaleReference)
+    {
+        yield return new WaitForSeconds(2);
+        initialScale = _parentScaleReference.localScale.x;
+        maxDelta = initialScale * 3;
     }
 
     public void PlayAnimation()
     {
         animator.Play("TextPanelAppear");
+    }
+
+
+    //Hide logic
+    Color color = Color.white;
+    Transform parentScaleReference;
+    private float initialScale;
+    [SerializeField] float maxDelta = 0.2f;
+
+    void Update()
+    {
+        float delta = parentScaleReference.localScale.x - initialScale;
+
+        if (delta > 0)
+        {
+            float newAlpha = Mathf.InverseLerp(maxDelta, 0, delta);
+            color.a = newAlpha;
+            backgroundColor.a = newAlpha;
+            foregroundColor.a = newAlpha;
+            textColor.a = newAlpha;
+            image.material.color = color;
+            foreground.material.color = foregroundColor;
+            background.material.color = backgroundColor;
+            title.color = textColor;
+            body.color = textColor;
+        }
     }
 }
